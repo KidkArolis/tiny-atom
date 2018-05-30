e06b808037eedb0be84b9b82ad2e7498c6f52644
Karolis Narkevicius <karolis.n@gmail.com>
2018-05-30 21:15:58 +0100

    Refactor the react bindings, simpler pure component implementation

--- src/react.js
+++ src/react.js
@@ -2,56 +2,52 @@ const React = require('react')
 
 const { Component, PureComponent, createContext } = React
 
-class PureConnect extends PureComponent {
-  render () {
-    const {
-      connectedComponent: ConnectedComponent,
-      connectedActions,
-      connectedDispatch,
-      ...mappedProps
-    } = this.props
-    const mappedActions = mapActions(connectedActions, connectedDispatch, mappedProps)
-    return <ConnectedComponent {...mappedProps} {...mappedActions} />
-  }
-}
-
 module.exports = function createAtomContext (atom) {
+  const { get, dispatch } = atom
   const Context = createContext()
 
   // connect is creates a HOC that will map props and bind actions
   // the component it creates is pure by default, and so if mapped
   // props or origina props hasn't changed, it doesn't rebind the
   // actions and it doesn't rerender the component
-  const connect = (map, actions, options = {}) => Component => originalProps => (
-    <Consumer>
-      {({ state, dispatch }) => {
-        const mappedProps = Object.assign({}, originalProps, map ? map(state, originalProps) : { state })
-        if (options.pure !== false) {
-          return (
-            <PureConnect
-              {...mappedProps}
-              connectedActions={actions}
-              connectedDispatch={dispatch}
-              connectedComponent={Component}
-            />
-          )
-        } else {
-          const mappedActions = mapActions(actions, dispatch, mappedProps)
-          return <Component {...mappedProps} {...mappedActions} />
+  function connect (map, actions, options = {}) {
+    if (typeof options.pure === 'undefined') {
+      options.pure = true
+    }
+
+    return function connectComponent (Component) {
+      class PureConnectedComponent extends PureComponent {
+        render () {
+          const mappedProps = this.props
+          const boundActions = bindActions(actions, mappedProps)
+          return <Component {...mappedProps} {...boundActions} />
         }
-      }}
-    </Consumer>
-  )
+      }
+
+      return function ConnectedComponent (originalProps) {
+        return (
+          <Context.Consumer>
+            {({ state }) => {
+              const mappedProps = Object.assign({}, originalProps, map ? map(state, originalProps) : { state })
+              return options.pure
+                ? <PureConnectedComponent {...mappedProps} />
+                : <Component {...mappedProps} {...bindActions(actions, mappedProps)} />
+            }}
+          </Context.Consumer>
+        )
+      }
+    }
+  }
 
   // Consumer is a wrapper around Context.Consumer with extra
   // props map and actions for transforming state and binding action fns
   function Consumer ({ map, actions, children }) {
     return (
       <Context.Consumer>
-        {({ state, dispatch }) => {
+        {({ state }) => {
           const mappedProps = map ? map(state) : { state }
-          const mappedActions = mapActions(actions, dispatch, mappedProps)
-          return children(Object.assign({}, mappedProps, mappedActions))
+          const boundActions = bindActions(actions, mappedProps)
+          return children(Object.assign({}, mappedProps, boundActions))
         }}
       </Context.Consumer>
     )
@@ -69,25 +65,25 @@ module.exports = function createAtomContext (atom) {
 
     render () {
       return (
-        <Context.Provider value={{ state: atom.get(), dispatch: atom.dispatch }}>
+        <Context.Provider value={{ state: get(), dispatch }}>
           {this.props.children}
         </Context.Provider>
       )
     }
   }
 
-  return { Provider, Consumer, connect }
-}
+  function bindActions (actions, mappedProps) {
+    if (!actions) {
+      return { dispatch }
+    }
+    if (typeof actions === 'function') {
+      return actions(dispatch, mappedProps)
+    }
+    return actions.reduce((acc, action) => {
+      acc[action] = payload => dispatch(action, payload)
+      return acc
+    }, {})
+  }
 
-function mapActions (actions, dispatch, mappedProps) {
-  if (!actions) {
-    return { dispatch }
-  }
-  if (typeof actions === 'function') {
-    return actions(dispatch, mappedProps)
-  }
-  return actions.reduce((acc, action) => {
-    acc[action] = payload => dispatch(action, payload)
-    return acc
-  }, {})
+  return { Provider, Consumer, connect }
 }
