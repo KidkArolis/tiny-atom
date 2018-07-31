74c7f91cc8ca507698a681322fdf1030f9501110
Karolis Narkevicius <karolis.n@gmail.com>
2018-07-31 16:13:53 +0100

    Remove context example - that's the default mode in atom 3

--- examples/react-context-example/atomContext.js
+++
@@ -1,9 +0,0 @@
-const createContext = require('tiny-atom/react/context')
-const createAtom = require('tiny-atom')
-const debug = require('tiny-atom/log')
-const actions = require('./actions')
-
-const atom = window.atom = createAtom({ count: 0 }, actions, { debug })
-const AtomContext = createContext(atom)
-
-module.exports = AtomContext
