a1513f6db5c845325e92140d7e8b8d3801615f1c
Karolis Narkevicius <karolis.n@gmail.com>
2018-07-31 16:58:48 +0100

    Improve React example

---
+++ examples/react-example/actions.js
@@ -0,0 +1,58 @@
+module.exports.initialState = {
+  todo: {
+    items: [
+      'learn tiny-atom',
+      'use tiny-atom',
+      'star tiny-atom'
+    ],
+    input: ''
+  },
+
+  hint: {
+    text: 'Type + hit enter',
+    show: true
+  },
+
+  analytics: {
+    events: []
+  }
+}
+
+module.exports.actions = {
+  addItem: ({ get, set, dispatch }, payload) => {
+    const { items, input } = get().todo
+    const nextItems = items.concat([input])
+    set({ todo: { items: nextItems, input: '' } })
+    dispatch('hideHint')
+    dispatch('trackEvent', { type: 'added' })
+  },
+
+  completeItem: ({ get, set, dispatch }, index) => {
+    const { items } = get().todo
+    const nextItems = items.filter((item, i) => i !== index)
+    set({ todo: { items: nextItems } })
+    dispatch('trackEvent', { user: 'anonymous', type: 'completed' })
+  },
+
+  updateItem: ({ set }, input) => {
+    set({ todo: { input } })
+  },
+
+  showHint: ({ set }) => {
+    set({ hint: { show: true } })
+  },
+
+  hideHint: ({ set }) => {
+    set({ hint: { show: false } })
+  },
+
+  trackEvent: ({ get, set }, event) => {
+    const { events } = get().analytics
+    const nextEvents = events.concat([event])
+    set({
+      analytics: {
+        events: nextEvents
+      }
+    })
+  }
+}
--- examples/react-example/index.js
+++ examples/react-example/index.js
@@ -1,92 +1,15 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 const createAtom = require('tiny-atom')
-const createConnector = require('tiny-atom/react')
-const devtools = require('tiny-atom/devtools')
+const { Provider } = require('tiny-atom/react')
 const log = require('tiny-atom/log')
+const App = require('./App')
+const { initialState, actions } = require('./actions')
 
-const atom = window.atom = createAtom({ count: 0, list: [1] }, actions(), {
-  debug: [log(), devtools()]
-})
-
-const { Provider, Consumer } = createConnector()
-
-function actions () {
-  return {
-    increment: ({ get, set }, payload) => {
-      set({ count: get().count + payload })
-    },
-
-    decrement: ({ get, set }, payload) => {
-      set({ count: get().count - payload })
-      const l = Math.round(Math.random() * 10)
-      set({ list: Array.from({length: l}, () => Math.floor(Math.random() * l)) })
-    },
-
-    asyncIncrement: ({ get, set, dispatch }, payload) => {
-      dispatch('asyncIncrementNested', payload)
-    },
-
-    asyncIncrementNested: ({ get, set, dispatch }, payload) => {
-      dispatch('increment', payload)
-      setTimeout(() => {
-        set({
-          count: get().count + payload,
-          extra: (get().extra || 'a') + 'a'
-        })
-        setTimeout(() => {
-          dispatch('decrement', 1)
-        }, 1000)
-      }, 1000)
-    },
-
-    track: (get, split, payload) => {
-      // track is a side effect, no store updates
-    }
-  }
-}
-
-const map = (state) => ({
-  doubleCount: state.count * 2,
-  count: state.count
-})
-
-const mapActions = [
-  'increment',
-  'decrement',
-  'asyncIncrement'
-]
-
-const App = () => (
-  <Provider atom={atom}>
-    <Consumer map={map} actions={mapActions}>
-      {({ count, doubleCount, asyncIncrement, increment, decrement }) => (
-        <div>
-          <h2>count: {count}</h2>
-          <h2>double: {doubleCount}</h2>
-          <Nested multiplier={5}>
-            <Nested multiplier={10} />
-          </Nested>
-          <button onClick={() => increment(1)}>Increment</button>
-          <button onClick={() => decrement(1)}>Decrement</button>
-          <button onClick={() => asyncIncrement(2)}>Async increment</button>
-        </div>
-      )}
-    </Consumer>
-  </Provider>
-)
-
-const Nested = ({ multiplier, children }) => (
-  <Consumer map={state => ({ count: state.count })}>
-    {({ count }) => (
-      <div style={{ padding: '30px' }}>
-        Nested component: { count * multiplier }
-        {children}
-      </div>
-    )}
-  </Consumer>
-)
+const atom = window.atom = createAtom(initialState, actions, { debug: log() })
 
 ReactDOM.render((
-  <App />
-), document.getElementById('root'))
+  <Provider atom={atom}>
+    <App />
+  </Provider>
+), document.querySelector('#root'))
