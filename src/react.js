ebde07c33cadca2907b0778bcf8018b20b9c3085
Tommy Kwan <tkwan196@gmail.com>
2018-10-27 01:19:52 +0100

    Add support for react hooks

--- src/react.js
+++ src/react.js
@@ -1,21 +1,50 @@
 const React = require('react')
+const { useContext, useState, useEffect } = React
 const raf = require('./raf')
 const printDebug = require('./debug')
 
 const dev = process.env.NODE_ENV !== 'production'
-const canUseDOM = !!(typeof window !== 'undefined' && window.document && window.document.createElement)
+const canUseDOM = !!(typeof window !== 'undefined' &&
+  window.document &&
+  window.document.createElement)
 
 function createContext () {
   const AtomContext = React.createContext()
 
-  class Provider extends React.Component {
-    render () {
-      return (
-        <AtomContext.Provider value={{ atom: this.props.atom, debug: this.props.debug }}>
-          {this.props.children}
-        </AtomContext.Provider>
-      )
-    }
+  function useAtom (selector, action, options) {
+    const value = useAtomState(selector, options)
+    const setValue = useAtomAction(action)
+    return [value, setValue]
+  }
+
+  function useAtomState (selector, options = {}) {
+    const { pure = true, observe = true } = options
+    const { atom } = useContext(AtomContext)
+    const [value, setValue] = useState(selector(atom.get()))
+
+    useEffect(() => {
+      if (!observe) return
+      return atom.observe(atom => {
+        const nextValue = selector(atom.get())
+        if (!pure || value !== nextValue) {
+          setValue(nextValue)
+        }
+      })
+    })
+    return value
+  }
+
+  function useAtomAction (action) {
+    const { atom } = useContext(AtomContext)
+    return payload => atom.dispatch(action, payload)
+  }
+
+  function Provider ({ atom, debug, children }) {
+    return (
+      <AtomContext.Provider value={{ atom, debug }}>
+        {children}
+      </AtomContext.Provider>
+    )
   }
 
   class ConsumerInner extends React.Component {
@@ -23,14 +52,20 @@ function createContext () {
       super()
       this.state = {}
       this.isPure = typeof props.pure === 'undefined' ? true : props.pure
-      this.shouldObserve = typeof props.observe === 'undefined' ? canUseDOM : props.observe
-      this.scheduleUpdate = props.sync ? () => this.update() : raf(() => this.update())
+      this.shouldObserve = typeof props.observe === 'undefined'
+        ? canUseDOM
+        : props.observe
+      this.scheduleUpdate = props.sync
+        ? () => this.update()
+        : raf(() => this.update())
     }
 
     observe () {
       if (!this.shouldObserve) return
       this.unobserve && this.unobserve()
-      this.unobserve = this.props.atom.observe(() => { this.cancelUpdate = this.scheduleUpdate() })
+      this.unobserve = this.props.atom.observe(() => {
+        this.cancelUpdate = this.scheduleUpdate()
+      })
       this.observedAtom = this.props.atom
       delete this.boundActions
       delete this.boundActionsSpec
@@ -52,7 +87,11 @@ function createContext () {
       // if it's <Consumer> with dynamic children, shortcut the check
       if (this.props.children !== nextProps.children) {
         if (dev && (this.context.debug || this.props.debug)) {
-          printDebug.children(this.debugName(), this.props.children, nextProps.children)
+          printDebug.children(
+            this.debugName(),
+            this.props.children,
+            nextProps.children
+          )
         }
         return true
       }
@@ -112,29 +151,39 @@ function createContext () {
       // do this in render, because:
       //  doing in constructor would cause memory leaks in SSR
       //  doing in componentDidMount leads to the wrong order of subscriptions
-      if (!this.unobserve || this.observedAtom !== this.props.atom) this.observe()
+      if (!this.unobserve || this.observedAtom !== this.props.atom) {
+        this.observe()
+      }
       const { atom, actions, originalProps, render, children } = this.props
       const mappedProps = this.state
       const boundActions = this.bindActions(actions, atom.dispatch, mappedProps)
-      return (render || children)(Object.assign({}, originalProps, mappedProps, boundActions))
+      return (render || children)(
+        Object.assign({}, originalProps, mappedProps, boundActions)
+      )
     }
   }
 
   ConsumerInner.getDerivedStateFromProps = (props, state) => {
     const { atom, originalProps, map } = props
-    return Object.assign({}, originalProps, map ? map(atom.get(), originalProps) : {})
+    return Object.assign(
+      {},
+      originalProps,
+      map ? map(atom.get(), originalProps) : {}
+    )
   }
 
   const Consumer = props => (
     <AtomContext.Consumer>
-      {({ atom, debug }) => <ConsumerInner {...props} atom={atom} debug={debug} />}
+      {({ atom, debug }) => (
+        <ConsumerInner {...props} atom={atom} debug={debug} />
+      )}
     </AtomContext.Consumer>
   )
 
   function connect (map, actions, options = {}) {
     return function connectComponent (Component) {
       const render = mappedProps => <Component {...mappedProps} />
-      const Connected = (props) => (
+      const Connected = props => (
         <Consumer
           displayName={Component.displayName || Component.name}
           map={map}
@@ -161,8 +210,23 @@ function createContext () {
     return false
   }
 
-  return { Provider, Consumer, connect }
+  return { Provider, Consumer, connect, useAtomState, useAtomAction, useAtom }
 }
 
-const { Provider, Consumer, connect } = createContext()
-module.exports = { Provider, Consumer, connect, createContext }
+const {
+  Provider,
+  Consumer,
+  connect,
+  useAtomState,
+  useAtomAction,
+  useAtom
+} = createContext()
+module.exports = {
+  Provider,
+  Consumer,
+  connect,
+  createContext,
+  useAtomState,
+  useAtomAction,
+  useAtom
+}
